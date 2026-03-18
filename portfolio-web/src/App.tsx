f92a149cfa252d7dff07c18d1a66a16ea3fb3200
ammon <ammonharps@gmail.com>
2026-03-17 21:20:10 -0600

    feat: add footer

--- portfolio-web/src/App.tsx
+++ portfolio-web/src/App.tsx
@@ -24,19 +24,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ammon Harps</h1>
       <TextTyper sentences={roles}/>
+      <div className='footer'>
+        <div className="read-the-docs">
+          Check out my github at <a href='https://github.com/ammonharps-git' target='_blank'>ammonharps-git</a> to see more of my projects.
+        </div>
+      </div>
     </>
   )
 }
