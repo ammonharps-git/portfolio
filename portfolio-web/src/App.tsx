039aabe1f8a16c5cfb30dd68cb59bae02fad1865
ammon <ammonharps@gmail.com>
2026-03-17 21:22:54 -0600

    cleanup: remove unused unused import and variables

--- portfolio-web/src/App.tsx
+++ portfolio-web/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,8 +11,6 @@ import TextTyper from './TextTyper.tsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
