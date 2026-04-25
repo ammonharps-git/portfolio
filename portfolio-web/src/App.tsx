7c32121f38cc315245d3ea88f00d67a06ac01dfa
ammon <ammonharps@gmail.com>
2026-04-25 16:11:07 -0600

    update: css and organizational changes to app level header and footer

--- portfolio-web/src/App.tsx
+++ portfolio-web/src/App.tsx
@@ -1,5 +1,4 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import simpleLogo from './assets/simple-logo.png'
 import './App.css'
 import TextTyper from './TextTyper.tsx'
 
@@ -14,20 +13,16 @@ function App() {
   return (
     <>
       <title>Ammon's Portfolio</title>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header'>
+        <img src={simpleLogo} className='header-logo' alt='Ammon Harps Logo'></img>
+        <div className='headerText'>Home</div>
+        <div className='headerText'>Contact Me</div>
+        {/* TODO: Make these nav links */}
       </div>
       <h1>Ammon Harps</h1>
       <TextTyper sentences={roles}/>
       <div className='footer'>
-        <div className="read-the-docs">
           Check out my github at <a href='https://github.com/ammonharps-git' target='_blank'>ammonharps-git</a> to see more of my projects.
-        </div>
       </div>
     </>
   )
