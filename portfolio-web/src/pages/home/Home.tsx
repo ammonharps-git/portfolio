bbf040426f3e85f5a6dabc36c242fe56d9b029c3
ammon <ammonharps@gmail.com>
2026-04-25 19:13:21 -0600

    feat: profile section of home page

--- portfolio-web/src/pages/home/Home.tsx
+++ portfolio-web/src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
 import TextTyper from '../../components/text-typer/TextTyper.tsx'
+import profileImage from '../../assets/profile-pic.jpg'
 import * as Constants from '../../Constants.ts'
 import './Home.css'
 
 function Home() {
   return (
     <>
-      <h1>{Constants.FULL_NAME}</h1>
-      <TextTyper sentences={Constants.ROLES}/>
+        <div className='profileContainer'>
+            <div className='nameTitleContainer'>
+                <div className='name'>{Constants.FULL_NAME}</div>
+                <TextTyper sentences={Constants.ROLES}/>
+            </div>
+            <img className='profileImage' src={profileImage} alt={`${Constants.FULL_NAME} profile picture`} />
+        </div>
     </>
   )
 }
