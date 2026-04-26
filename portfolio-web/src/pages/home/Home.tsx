36e289c0714df88f540149e8891cdf872ec479e2
ammon <ammonharps@gmail.com>
2026-04-25 22:45:02 -0600

    feat: first draft of skills section

--- portfolio-web/src/pages/home/Home.tsx
+++ portfolio-web/src/pages/home/Home.tsx
@@ -1,11 +1,42 @@
 import TextTyper from '../../components/text-typer/TextTyper.tsx'
 import profileImage from '../../assets/profile-pic.jpg'
+import { Logos } from './ImageImporter.ts'
 import * as Constants from '../../Constants.ts'
 import './Home.css'
 
 function Home() {
+  class LabeledLogo {
+    public image : string;
+
+    public label : string;
+
+    public labelBackgroundColor : string;
+
+    public textColorOverride : string | undefined;
+
+    constructor(image: string, label : string, labelColor: string, textColorOverride?: string) {
+      this.image = image;
+      this.label = label;
+      this.labelBackgroundColor = labelColor;
+      this.textColorOverride = textColorOverride;
+    }
+  }
+
+  const skills : LabeledLogo[] = [
+    new LabeledLogo(Logos.html, "HTML", "#ff4b00"),
+    new LabeledLogo(Logos.css, "CSS", "#417aff"),
+    new LabeledLogo(Logos.csharp, "C#", "#62397b"),
+    new LabeledLogo(Logos.cpp, "C++", "#4462b3"),
+    new LabeledLogo(Logos.c, "C", "#4462b3"),
+    new LabeledLogo(Logos.typescript, "Typescript", "#587cff"),
+    new LabeledLogo(Logos.javascript, "JavaScript", "#fffc44", "#000000"),
+    new LabeledLogo(Logos.rust, "Rust", "#515151"),
+    new LabeledLogo(Logos.python, "Python", "#e4b227", "#000000"),
+    new LabeledLogo(Logos.java, "Java", "#ff8b43"),
+  ];
+
   return (
-    <>
+    <div className='sectionsContainer'>
         <div className='profileContainer'>
             <div className='nameTitleContainer'>
                 <div className='name'>{Constants.FULL_NAME}</div>
@@ -13,7 +44,19 @@ function Home() {
             </div>
             <img className='profileImage' src={profileImage} alt={`${Constants.FULL_NAME} profile picture`} />
         </div>
-    </>
+        <div className='skillsContainer'>
+          {
+            skills.map(skill => (
+            <div className='skillCard'>
+              <img className='skillImage' src={skill.image}/>
+              <div className='skillLabel' style={{background: skill.labelBackgroundColor, color: skill.textColorOverride}}>
+                {skill.label}
+              </div>
+            </div>
+            ))
+          }
+        </div>
+    </div>
   )
 }
 
---
+++ portfolio-web/src/pages/home/ImageImporter.ts
@@ -0,0 +1,12 @@
+import html from '../../assets/language-logos/html5.svg'
+import css from '../../assets/language-logos/css.png'
+import typescript from '../../assets/language-logos/typescript.svg'
+import javascript from '../../assets/language-logos/javascript.jpg'
+import c from '../../assets/language-logos/c.svg'
+import cpp from '../../assets/language-logos/cpp.png'
+import csharp from '../../assets/language-logos/csharp.svg'
+import java from '../../assets/language-logos/java.png'
+import rust from '../../assets/language-logos/rust.png'
+import python from '../../assets/language-logos/python.svg'
+
+export const Logos = { html, css, typescript, javascript, c, cpp, csharp, java, rust, python };
